40dfd74800f46dda2d04904bc4b7c51e6b539b3c
Alaa Sarhan <sarhan.alaa@protonmail.com>
2018-11-07 02:15:59 +0100

    convert tabs to spaces

--- src/Provider/Docusign.php
+++ src/Provider/Docusign.php
@@ -10,94 +10,94 @@ use Psr\Http\Message\ResponseInterface;
 
 class Docusign extends AbstractProvider
 {
-	use BearerAuthorizationTrait {
-		getAuthorizationHeaders as getTokenBearerAuthorizationHeaders;
-	}
-	
-	const URL_ROOT = 'https://account.docusign.com/oauth';
-	const URL_ROOT_SANDBOX = 'https://account-d.docusign.com/oauth';
+    use BearerAuthorizationTrait {
+        getAuthorizationHeaders as getTokenBearerAuthorizationHeaders;
+    }
+    
+    const URL_ROOT = 'https://account.docusign.com/oauth';
+    const URL_ROOT_SANDBOX = 'https://account-d.docusign.com/oauth';
 
-	const ENDPOINT_AUTHORIZTION = 'auth';
-	const ENDPOINT_ACCESS_TOKEN = 'token';
-	const ENDPOINT_RESOURCE_OWNER_DETAILS = 'userinfo';
+    const ENDPOINT_AUTHORIZTION = 'auth';
+    const ENDPOINT_ACCESS_TOKEN = 'token';
+    const ENDPOINT_RESOURCE_OWNER_DETAILS = 'userinfo';
 
-	const SCOPE_SIGNATURE = 'signature';
-	const SCOPE_EXTENDED = 'extended';
-	const SCOPE_IMPERSONATION = 'impersonation';
-	const SCOPES_DEFAULT = [
-		self::SCOPE_SIGNATURE,
-		self::SCOPE_EXTENDED
-	];
-	const SCOPES_SEPARATOR = ' ';
+    const SCOPE_SIGNATURE = 'signature';
+    const SCOPE_EXTENDED = 'extended';
+    const SCOPE_IMPERSONATION = 'impersonation';
+    const SCOPES_DEFAULT = [
+        self::SCOPE_SIGNATURE,
+        self::SCOPE_EXTENDED
+    ];
+    const SCOPES_SEPARATOR = ' ';
 
-	protected $sandbox = false;
+    protected $sandbox = false;
 
-	/**
-	 * @inheritDoc
-	 */
-	public function getBaseAuthorizationUrl() : string
-	{
-		return $this->getUrl(self::ENDPOINT_AUTHORIZTION);
-	}
+    /**
+     * @inheritDoc
+     */
+    public function getBaseAuthorizationUrl() : string
+    {
+        return $this->getUrl(self::ENDPOINT_AUTHORIZTION);
+    }
 
-	/**
-	 * @inheritDoc
-	 */
-	public function getBaseAccessTokenUrl(array $params) : string
-	{
-		return $this->getUrl(self::ENDPOINT_ACCESS_TOKEN);
-	}
+    /**
+     * @inheritDoc
+     */
+    public function getBaseAccessTokenUrl(array $params) : string
+    {
+        return $this->getUrl(self::ENDPOINT_ACCESS_TOKEN);
+    }
 
-	/**
-	 * @inheritDoc
-	 */
-	public function getResourceOwnerDetailsUrl(AccessToken $token) : string
-	{
-		return $this->getUrl(self::ENDPOINT_RESOURCE_OWNER_DETAILS);
-	}
+    /**
+     * @inheritDoc
+     */
+    public function getResourceOwnerDetailsUrl(AccessToken $token) : string
+    {
+        return $this->getUrl(self::ENDPOINT_RESOURCE_OWNER_DETAILS);
+    }
 
-	public function getUrl($path)
-	{
-		return sprintf(
-			'%s/%s',
-			$this->sandbox ? self::URL_ROOT_SANDBOX : self::URL_ROOT,
-			$path
-		);
-	}
+    public function getUrl($path)
+    {
+        return sprintf(
+            '%s/%s',
+            $this->sandbox ? self::URL_ROOT_SANDBOX : self::URL_ROOT,
+            $path
+        );
+    }
 
-	protected function getDefaultScopes()
-	{
-		return self::SCOPES_DEFAULT;
-	}
+    protected function getDefaultScopes()
+    {
+        return self::SCOPES_DEFAULT;
+    }
 
-	protected function checkResponse(ResponseInterface $response, $data)
-	{
-		if ($response->getStatusCode() >= 400) {
+    protected function checkResponse(ResponseInterface $response, $data)
+    {
+        if ($response->getStatusCode() >= 400) {
             throw new IdentityProviderException(
                 $response->getReasonPhrase(),
                 $response->getStatusCode(),
                 $response
             );
         }
-	}
+    }
 
-	protected function createResourceOwner(array $response, AccessToken $token)
-	{
-		return new DocusignUser($response, $token);
-	}
+    protected function createResourceOwner(array $response, AccessToken $token)
+    {
+        return new DocusignUser($response, $token);
+    }
 
-	protected function getScopeSeparator()
+    protected function getScopeSeparator()
     {
         return self::SCOPES_SEPARATOR;
     }
 
     protected function getDefaultHeaders()
     {
-    	return ['Authorization' => 'Basic ' . $this->getBasicAuth()];
+        return ['Authorization' => 'Basic ' . $this->getBasicAuth()];
     }
 
-	private function getBasicAuth() : string
-	{
-		return base64_encode(sprintf('%s:%s', $this->clientId, $this->clientSecret));
-	}
+    private function getBasicAuth() : string
+    {
+        return base64_encode(sprintf('%s:%s', $this->clientId, $this->clientSecret));
+    }
 }
